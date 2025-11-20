82d1eca415b54234bf35758887aabe0f90d0e62f
gingrspacecadet <jackrawlings2011@gmail.com>
2025-11-20 08:16:33 +0000

    Update ram.h

--- emu/ram.h
+++ emu/ram.h
@@ -4,6 +4,18 @@
 #include <stdint.h>
 #include "machine.h"
 
+typedef struct Page {
+    uint32_t data[WORDS_PER_PAGE];
+} Page;
+
+typedef struct Node {
+    uint32_t page_num;
+    Page *page;
+    struct Node *next;
+} Node;
+
+Node* page_table = NULL;
+
 void ram_init(void);
 void ram_free(void);
 void ram_write(uint32_t addr, uint32_t value);
