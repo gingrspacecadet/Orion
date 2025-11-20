ced4fe44b42d63215f5e74d49e21c568a673e2b0
gingrspacecadet <jackrawlings2011@gmail.com>
2025-11-20 08:13:13 +0000

    Update ram.h

--- emu/ram.h
+++ emu/ram.h
@@ -2,8 +2,9 @@
 #define RAM_H
 
 #include <stdint.h>
+#include "machine.h"
 
-void ram_init(void);
+void ram_init(Machine* m);
 void ram_free(void);
 void ram_write(uint32_t addr, uint32_t value);
 uint32_t ram_read(uint32_t addr);
