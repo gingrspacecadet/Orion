92ed862746c91848511d76c88804e7c0d4a2ec7c
gingrspacecadet <jackrawlings2011@gmail.com>
2026-05-30 09:43:27 +0100

    termios handler broken

--- emu/uart.c
+++ emu/uart.c
@@ -1,14 +1,15 @@
+#include <sys/select.h>
+#include <termios.h>
 #include <unistd.h>
 #include <stdint.h>
-#include <termios.h>
-#include <sys/select.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-static struct termios oldt, newt;
+static struct termios oldt;
 static int termios_initialized = 0;
 
 static void restore_termios(void) {
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &oldt);
 }
 
 static void init_termios(void) {
@@ -17,13 +18,13 @@ static void init_termios(void) {
 
     termios_initialized = 1;
 
+    struct termios t;
     tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-
-    // Disable canonical mode and echo
-    newt.c_lflag &= ~(ICANON | ECHO);
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    t = oldt;
+    t.c_cflag &= ~(ICANON | ECHO);
+    t.c_cc[VMIN] = 1;
+    t.c_cc[VTIME] = 0;
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &t);
 
     atexit(restore_termios);
 }
